17698f50086022592b7e107917cc4b118c28c4b0
MANU-KARTHIK-K-S <manukarthik097@gmail.com>
2023-12-06 20:37:19 +0530

    2021 whatsapp clone

---
+++ lib/Screens/CreateGroup.dart
@@ -0,0 +1,129 @@
+import 'package:chatapp/CustomUI/AvtarCard.dart';
+import 'package:chatapp/CustomUI/ButtonCard.dart';
+import 'package:chatapp/CustomUI/ContactCard.dart';
+import 'package:chatapp/Model/ChatModel.dart';
+import 'package:flutter/material.dart';
+
+class CreateGroup extends StatefulWidget {
+  CreateGroup({Key key}) : super(key: key);
+
+  @override
+  _CreateGroupState createState() => _CreateGroupState();
+}
+
+class _CreateGroupState extends State<CreateGroup> {
+  List<ChatModel> contacts = [
+    ChatModel(name: "Manu Stack", status: "A full stack developer"),
+    ChatModel(name: "Karthik", status: "Flutter Developer..........."),
+    ChatModel(name: "manjnu", status: "Web developer..."),
+    ChatModel(name: "mahi ", status: "App developer...."),
+    ChatModel(name: "geetha", status: "Raect developer.."),
+    ChatModel(name: "sivan", status: "Full Stack Web"),
+    ChatModel(name: "william", status: "Example work"),
+    ChatModel(name: "Minnal Murali", status: "Sharing is caring"),
+    ChatModel(name: "Iron", status: "....."),
+    ChatModel(name: "Loki", status: "Love you Mom Dad"),
+    ChatModel(name: "Thor", status: "I find the bugs"),
+  ];
+  List<ChatModel> groupmember = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "New Group",
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "Add participants",
+                style: TextStyle(
+                  fontSize: 13,
+                ),
+              )
+            ],
+          ),
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.search,
+                  size: 26,
+                ),
+                onPressed: () {}),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Color(0xFF128C7E),
+            onPressed: () {},
+            child: Icon(Icons.arrow_forward)),
+        body: Stack(
+          children: [
+            ListView.builder(
+                itemCount: contacts.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return Container(
+                      height: groupmember.length > 0 ? 90 : 10,
+                    );
+                  }
+                  return InkWell(
+                    onTap: () {
+                      setState(() {
+                        if (contacts[index - 1].select == true) {
+                          groupmember.remove(contacts[index - 1]);
+                          contacts[index - 1].select = false;
+                        } else {
+                          groupmember.add(contacts[index - 1]);
+                          contacts[index - 1].select = true;
+                        }
+                      });
+                    },
+                    child: ContactCard(
+                      contact: contacts[index - 1],
+                    ),
+                  );
+                }),
+            groupmember.length > 0
+                ? Align(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 75,
+                          color: Colors.white,
+                          child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: contacts.length,
+                              itemBuilder: (context, index) {
+                                if (contacts[index].select == true)
+                                  return InkWell(
+                                    onTap: () {
+                                      setState(() {
+                                        groupmember.remove(contacts[index]);
+                                        contacts[index].select = false;
+                                      });
+                                    },
+                                    child: AvatarCard(
+                                      chatModel: contacts[index],
+                                    ),
+                                  );
+                                return Container();
+                              }),
+                        ),
+                        Divider(
+                          thickness: 1,
+                        ),
+                      ],
+                    ),
+                    alignment: Alignment.topCenter,
+                  )
+                : Container(),
+          ],
+        ));
+  }
+}
---
+++ lib/Screens/SelectContact.dart
@@ -0,0 +1,112 @@
+import 'package:chatapp/CustomUI/ButtonCard.dart';
+import 'package:chatapp/CustomUI/ContactCard.dart';
+import 'package:chatapp/Model/ChatModel.dart';
+import 'package:chatapp/Screens/CreateGroup.dart';
+import 'package:flutter/material.dart';
+
+class SelectContact extends StatefulWidget {
+  SelectContact({Key key}) : super(key: key);
+
+  @override
+  _SelectContactState createState() => _SelectContactState();
+}
+
+class _SelectContactState extends State<SelectContact> {
+  @override
+  Widget build(BuildContext context) {
+    List<ChatModel> contacts = [
+      ChatModel(name: "Manu", status: "A full stack developer"),
+      ChatModel(name: "Karthik", status: "Flutter Developer..........."),
+      ChatModel(name: "manjnu", status: "Web developer..."),
+      ChatModel(name: "mahi", status: "App developer...."),
+      ChatModel(name: "geetha", status: "Raect developer.."),
+      ChatModel(name: "sivan", status: "Full Stack Web"),
+      ChatModel(name: "william", status: "Example work"),
+      ChatModel(name: "Minnal Murali", status: "Sharing is caring"),
+      ChatModel(name: "Iron Man", status: "....."),
+      ChatModel(name: "Loki", status: "Love you Mom Dad"),
+      ChatModel(name: "Thor", status: "I find the bugs"),
+    ];
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Select Contact",
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "256 contacts",
+                style: TextStyle(
+                  fontSize: 13,
+                ),
+              )
+            ],
+          ),
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.search,
+                  size: 26,
+                ),
+                onPressed: () {}),
+            PopupMenuButton<String>(
+              padding: EdgeInsets.all(0),
+              onSelected: (value) {
+                print(value);
+              },
+              itemBuilder: (BuildContext contesxt) {
+                return [
+                  PopupMenuItem(
+                    child: Text("Invite a friend"),
+                    value: "Invite a friend",
+                  ),
+                  PopupMenuItem(
+                    child: Text("Contacts"),
+                    value: "Contacts",
+                  ),
+                  PopupMenuItem(
+                    child: Text("Refresh"),
+                    value: "Refresh",
+                  ),
+                  PopupMenuItem(
+                    child: Text("Help"),
+                    value: "Help",
+                  ),
+                ];
+              },
+            ),
+          ],
+        ),
+        body: ListView.builder(
+            itemCount: contacts.length + 2,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (builder) => CreateGroup()));
+                  },
+                  child: ButtonCard(
+                    icon: Icons.group,
+                    name: "New group",
+                  ),
+                );
+              } else if (index == 1) {
+                return ButtonCard(
+                  icon: Icons.person_add,
+                  name: "New contact",
+                );
+              }
+              return ContactCard(
+                contact: contacts[index - 2],
+              );
+            }));
+  }
+}
